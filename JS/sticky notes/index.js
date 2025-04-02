085af0749a37299e4a58a9ab52515d54bb46b091
deeplearn009 <sragimov@vk.com>
2025-04-02 21:42:43 +0400

    fixed

--- JS/sticky notes/index.js
+++ JS/sticky notes/index.js
@@ -1,36 +1,54 @@
-const addNoteBtn = document.getElementById("note")
-const colorBtns = document.querySelectorAll(".colors")
-const btn = document.getElementById("first")
-const noteList = document.getElementById("note-list")
+// const addNoteBtn = document.getElementById("note")
+// const colorBtns = document.querySelectorAll(".colors")
+// const btn = document.getElementById("first")
+// const noteList = document.getElementById("note-list")
 
-let currentColor = 'yellow'
+// let currentColor = 'yellow'
 
-addNoteBtn.addEventListener("click", createNewNote)
+// addNoteBtn.addEventListener("click", createNewNote)
 
 // colorBtns.forEach(btn => {
-//     btn.addEventListener("click", () => {
-//         currentColor = btn.style.backgroundColor;
+//      btn.addEventListener("click", () => {
+//          currentColor = btn.style.backgroundColor;
 //         console.log(colorBtns);
         
-//     })
+//      })
 // })
 
+// function createNewNote(content = '', bgColor = currentColor) {
+//     const newList = document.createElement('li')
+//     const newNote = document.createElement('textarea')
+    
+//     newNote.style.backgroundColor = bgColor;
+
+    
+
+//     newList.appendChild(newNote)
+//     noteList.appendChild(newList)
+// }
+
+
+const addNoteBtn = document.getElementById("note");
+const colorButtons = document.querySelectorAll(".colors button");
+const noteList = document.getElementById("note-list");
+
+let currentColor = 'yellow';
+
+addNoteBtn.addEventListener("click", createNewNote);
+
+colorButtons.forEach(btn => {
+    btn.addEventListener("click", () => {
+        currentColor = window.getComputedStyle(btn).backgroundColor;
+        // console.log(currentColor);
+    });
+});
+
 function createNewNote(content = '', bgColor = currentColor) {
-    const newList = document.createElement('li')
-    const newNote = document.createElement('textarea')
+    const newList = document.createElement('li');
+    const newNote = document.createElement('textarea');
     
     newNote.style.backgroundColor = bgColor;
-
     
-
-    newList.appendChild(newNote)
-    noteList.appendChild(newList)
+    newList.appendChild(newNote);
+    noteList.appendChild(newList);
 }
-
-btn.addEventListener("click", () => {
-      console.log(typeof currentColor);
-    currentColor = btn.style.backgroundColor;
-  
-    
-})
-
